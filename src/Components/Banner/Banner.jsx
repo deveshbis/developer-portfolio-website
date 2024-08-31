6283790411677658cf73df3d82eb8ed67f7b2af7
deveshbis <deveshbiswas.cse@gmail.com>
2024-08-31 20:07:04 +0600

    project update

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -26,7 +26,7 @@ const Banner = () => {
                         </h6>
                     </div>
 
-                    <div className="flex justify-end items-center p-2 bg-gradient-to-b from-[#4e0083] to-[#796089] rounded-bl-[230px] w-full h-[500px]">
+                    <div className="flex justify-end items-center p-2 bg-gradient-to-b from-[#4e0083] to-[#796089] rounded-bl-[230px] w-full lg:h-[500px] h-full">
                         <Zoom duration={3000}>
                             <div className="h-72 w-72 rounded-full bg-gradient-to-tr from-[#4e0083] to-[#c19ed6] p-5">
                                 <img src={proflePic} className="w-full h-full rounded-full object-cover border-8 border-white" alt="img" />
@@ -35,8 +35,8 @@ const Banner = () => {
                     </div>
                 </div>
 
-                <div className="absolute -top-[5px] -left-[5px] w-48 h-48 rounded-full bg-[#4e0083] opacity-40 shadow-lg"></div>
-                <div className="absolute -top-10 -left-10 w-48 h-48 rounded-full bg-[#4e0083] opacity-40 shadow-lg"></div>
+                <div className="absolute lg:-top-[5px] lg:-left-[5px] -top-[50px] -left-[50px] lg:w-48 lg:h-48 w-28 h-28  rounded-full bg-[#4e0083] opacity-40 shadow-lg"></div>
+                <div className="absolute -top-10 -left-10 lg:w-48 lg:h-48 w-28 h-28 rounded-full bg-[#4e0083] opacity-40 shadow-lg"></div>
             </div>
 
 
