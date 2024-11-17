3d073aa9d8ce6fe6c33e933ebc2bb65285abe367
deveshbis <deveshbiswas.cse@gmail.com>
2024-11-17 13:52:50 +0600

    updated profile pic

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,5 +1,5 @@
 
-import { Fade, Zoom } from 'react-awesome-reveal';
+import { Fade,} from 'react-awesome-reveal';
 import proflePic from '../../assets/devesh.jpg';
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 
@@ -27,33 +27,18 @@ const Banner = () => {
                     </div>
 
                     <div className="flex justify-end items-center p-2 bg-gradient-to-b from-[#4e0083] to-[#796089] rounded-bl-[230px] w-full lg:h-[500px] h-full">
-                        <Zoom duration={3000}>
+                        {/* <Zoom duration={3000}>
                             <div className="h-72 w-72 rounded-full bg-gradient-to-tr from-[#4e0083] to-[#c19ed6] p-5">
                                 <img src={proflePic} className="w-full h-full rounded-full object-cover border-8 border-white" alt="img" />
                             </div>
-                        </Zoom>
+                        </Zoom> */}
+                        <img src={proflePic} className="w-1/2 h-full object-cover rounded-lg border-8 border-white" alt="img" />
                     </div>
                 </div>
 
                 <div className="absolute lg:-top-[5px] lg:-left-[5px] -top-[50px] -left-[50px] lg:w-48 lg:h-48 w-28 h-28  rounded-full bg-[#4e0083] opacity-40 shadow-lg"></div>
                 <div className="absolute -top-10 -left-10 lg:w-48 lg:h-48 w-28 h-28 rounded-full bg-[#4e0083] opacity-40 shadow-lg"></div>
             </div>
-            {/* <div className="text-white flex w-full justify-between items-start p-10 md:p-20">
-                <div className="md:w-2/4 md:pt-10 space-y-3">
-                    <h1 className="text-xl md:text-6xl font-bold flex leading-normal tracking-tighter">
-                        
-                    </h1>
-                    <p className="text-sm md:text-2xl tracking-tight ">
-                        Hello! 👋 I am a dedicated Front-End Web Developer. I specialize in crafting engaging and responsive web experiences, blending creativity with technical proficiency to deliver seamless interfaces that captivate and delight users.
-                    </p>
-                    <button className="mt-5 md:md-10 text-white py02 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]">
-                        Contact Me
-                    </button>
-                </div>
-                <div>
-                    <img className="" src={proflePic} alt="" />
-                </div>
-            </div> */}
         </div>
     );
 };
